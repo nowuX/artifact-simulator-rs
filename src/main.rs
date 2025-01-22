db961b8e8c032fb2fa70596798fe366e6f9ceb04
Nowu <nowu.user@gmail.com>
2025-01-21 21:31:09 -0500

    fix: clean main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
 use ras::Artifact;
 
 fn main() {
-    loop {
-        let artifact = Artifact::random();
-        print!("\x1B[2J\x1B[1;1H");
-        println!("\n{}", artifact);
-        std::thread::sleep(std::time::Duration::from_millis(500));
-    }
+    let artifact = Artifact::random();
+    println!("\n{}", artifact);
 }
